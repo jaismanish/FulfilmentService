ff17a45f3124dccb39c42685b9c5e9ae32f3988e
Manish Jaiswal <manish.jaiswal_ftc@external.swiggy.in>
2024-03-12 17:49:11 +0530

    Added test for User Controller

--- src/main/java/FulfilmentService/dto/Address.java
+++ src/main/java/FulfilmentService/dto/Address.java
@@ -1,4 +1,40 @@
 package FulfilmentService.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
 public class Address {
+    @Min(value = 1)
+    @JsonAlias(value = "Building")
+    private int building;
+
+    @JsonAlias(value = "Floor")
+    private String floor;
+
+    @JsonAlias(value = "Locality")
+    private String locality;
+
+    @JsonAlias(value = "City")
+    private String city;
+
+    @JsonAlias(value = "State")
+    private String state;
+
+    @JsonAlias(value = "Country")
+    private String country;
+
+    @Pattern(regexp = "^[1-9]\\d{5}$")
+    @JsonAlias(value = "Zipcode")
+    private String pinCode;
 }
---
+++ src/test/java/FulfilmentService/controllers/UserControllerTest.java
@@ -0,0 +1,81 @@
+package FulfilmentService.controllers;
+
+import FulfilmentService.dto.Address;
+import FulfilmentService.exceptions.UserAlreadyRegistered;
+import FulfilmentService.models.RegistrationRequest;
+import FulfilmentService.services.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class UserControllerTest {
+
+    @MockBean
+    private UserService userService;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    Address address = new Address();
+    RegistrationRequest registrationRequest = new RegistrationRequest();
+    String request;
+
+    @BeforeEach
+    void setup() throws JsonProcessingException {
+        reset(userService);
+        address = Address.builder()
+                .building(1)
+                .floor("floor")
+                .locality("locality")
+                .city("city")
+                .state("state")
+                .country("country")
+                .pinCode("560068")
+                .build();
+
+        registrationRequest = RegistrationRequest.builder()
+                .name("name")
+                .username("username")
+                .password("password")
+                .address(address)
+                .build();
+        request = objectMapper.writeValueAsString(registrationRequest);
+
+    }
+
+
+    @Test
+    void testRegisterUser() throws Exception {
+        when(userService.register(registrationRequest)).thenReturn(new ResponseEntity<>(HttpStatus.CREATED));
+        mvc.perform(post("/api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(request)
+        ).andExpect(status().isCreated());
+        verify(userService, times(1)).register(registrationRequest);
+    }
+
+    @Test
+    void testRegisterUserAlreadyRegistered_ShouldThrowUserAlreadyRegistered() throws Exception {
+        when(userService.register(registrationRequest)).thenThrow(new UserAlreadyRegistered());
+        mvc.perform(post("/api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(request)
+        ).andExpect(status().isBadRequest());
+        verify(userService, times(1)).register(registrationRequest);
+    }
+
+}
