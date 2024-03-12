a70b42c30c5a6847d95eb7470de689d61e10e5ec
Manish Jaiswal <manish.jaiswal_ftc@external.swiggy.in>
2024-03-12 16:00:47 +0530

    Added Updated fields in User entity

---
+++ src/main/java/FulfilmentService/dto/Address.java
@@ -0,0 +1,4 @@
+package FulfilmentService.dto;
+
+public class Address {
+}
--- src/main/java/FulfilmentService/entities/User.java
+++ src/main/java/FulfilmentService/entities/User.java
@@ -1,11 +1,39 @@
 package FulfilmentService.entities;
 
+import FulfilmentService.dto.Address;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private Address address;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
